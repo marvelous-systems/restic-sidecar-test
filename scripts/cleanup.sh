6f31ab9ca51283e58117d969766de0a17836d0f4
Noah Hummel <n.p.hummel@gmail.com>
2019-01-30 02:38:04 +0100

    [FEATURE] create & cleanup temp dir

---
+++ scripts/cleanup.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+cd ..
+rm -rf "$WORKDIR" 
--- test.sh
+++ test.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 
-TEMPDIR=$(mktemp)
-
+# . ./scripts/check-args.sh
 . ./scripts/check-docker-is-running.sh
-. ./scripts/prepare-payload.sh
-. ./scripts/perform-backup.sh
-. ./scripts/restore-backup.sh
-. ./scripts/validate-restore.sh
+
+PROJECT_NAME="marvelous-systems.restic-sidecar-test"
+WORKDIR="$(pwd)/.${PROJECT_NAME}"
+
+mkdir "$WORKDIR"
+cd "$WORKDIR"
+
+# . ../scripts/prepare-data.sh
+# . ../scripts/perform-backup.sh
+# . ../scripts/alter-data.sh
+# . ../scripts/restore-backup.sh
+# . ../scripts/validate-data.sh
+. ../scripts/cleanup.sh
