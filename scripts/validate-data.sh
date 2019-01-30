b07ab13a1ae1839868ed1da00f5c564e71d8cea3
Noah Hummel <n.p.hummel@gmail.com>
2019-01-30 03:13:08 +0100

    [FEATURE] validate data

--- scripts/validate-data.sh
+++ scripts/validate-data.sh
@@ -1,2 +1,15 @@
 #!/bin/sh
 echo "Validating restored files..."
+
+for i in $(seq 1 "$N")
+do
+	if [[ ! -f "./$i" ]]; then
+		# echo "[Debug] File $i does not exist."
+		RET=$((RET + 1))
+	elif cmp -s "./$i" "./$i.doppelganger"; then
+		RET=$((RET))
+	else
+		# echo "[Debug] File $i is modified."
+		RET=$((RET + 1))
+	fi
+done
--- test.sh
+++ test.sh
@@ -3,6 +3,8 @@
 . ./scripts/check-args.sh
 . ./scripts/check-docker-is-running.sh
 
+RET=0
+
 PROJECT_NAME="marvelous-systems.restic-sidecar-test"
 WORKDIR="$(pwd)/.${PROJECT_NAME}"
 
@@ -13,5 +15,11 @@ cd "$WORKDIR"
 . ../scripts/perform-backup.sh
 . ../scripts/alter-data.sh
 # . ../scripts/restore-backup.sh
-# . ../scripts/validate-data.sh
+. ../scripts/validate-data.sh
 . ../scripts/cleanup.sh
+
+if [[ RET != 0 ]]; then
+	echo "There were errors!"
+fi
+
+exit "$RET"
